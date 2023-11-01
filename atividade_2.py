b20a5ffddeff952982ffe785efdbdff66d47ddee
brndeveloper <136709268+brndeveloper@users.noreply.github.com>
2023-11-01 15:26:37 -0300

    deleted example code atividade_2.py

--- atividade_2.py
+++ atividade_2.py
@@ -4,5 +4,3 @@ def one_in_two(list1, list2):
         if element_1 not in list2 and element_1 not in one_in_two_list:
             one_in_two_list.append(element_1)
     return f'Lista com elementos diferentes: {one_in_two_list}'
-
-print(one_in_two([3, 5, 6, 1, 2, 3], [2, 1, 8, 6, 5, 9, 3]))
