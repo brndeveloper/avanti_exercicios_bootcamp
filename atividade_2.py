adcd2fe3cb28764fa666fcb8cedb56be6719fca3
brndeveloper <136709268+brndeveloper@users.noreply.github.com>
2023-11-01 15:25:51 -0300

    fix if unnecessary atividade_2.py

--- atividade_2.py
+++ atividade_2.py
@@ -3,8 +3,6 @@ def one_in_two(list1, list2):
     for element_1 in list1:
         if element_1 not in list2 and element_1 not in one_in_two_list:
             one_in_two_list.append(element_1)
-    if len(one_in_two_list) == 0:
-        return 'As listas não '
     return f'Lista com elementos diferentes: {one_in_two_list}'
 
 print(one_in_two([3, 5, 6, 1, 2, 3], [2, 1, 8, 6, 5, 9, 3]))
